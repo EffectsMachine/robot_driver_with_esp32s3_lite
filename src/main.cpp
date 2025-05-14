30e05a7966c11d4938dc15eb2216489200141f9a
EffectsMachine <effectsmachine@outlook.com>
2025-05-14 20:33:26 +0800

    Refactor speed control logic in loop function: adjust speed calculation for smoother operation and remove unused speed mode variables

--- src/main.cpp
+++ src/main.cpp
@@ -806,21 +806,21 @@ void loop() {
 
     float speed_limit = 1.0;
     if (sbusData.ch[4] < 300) {
-      spd_mode = 1;
       speed_limit = 0.33;
     } else if (sbusData.ch[4] == 1002) {
-      spd_mode = 2;
       speed_limit = 0.66;
     } else if (sbusData.ch[4] > 1700) {
-      spd_mode = 3;
       speed_limit = 1.0;
     }
 
     float speed_input = constrain((sbusData.ch[2] - 992.0)/720.0, -1.0, 1.0);
     float turn_input = - constrain((sbusData.ch[3] - 992.0)/720.0, -1.0, 1.0);
 
-    float left_speed = (speed_input - turn_input) * speed_limit * 6000.0;
-    float right_speed = (speed_input + turn_input) * speed_limit * 6000.0;
+    // float left_speed = (speed_input - turn_input) * speed_limit * 6000.0;
+    // float right_speed = (speed_input + turn_input) * speed_limit * 6000.0;
+
+    float left_speed = (speed_input) * speed_limit * 6000.0 - (turn_input * 0.33 * 6000.0);
+    float right_speed = (speed_input) * speed_limit * 6000.0 + (turn_input * 0.33 * 6000.0);
 
     jointsCtrl.hubMotorCtrl(left_speed, -(right_speed), -right_speed, left_speed);
   }
